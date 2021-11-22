21ccf18ee9f00f163a3701471905e7a51edf054b
h510-cell <58731327+h510-cell@users.noreply.github.com>
2021-11-22 10:49:21 +0530

    Add files via upload

---
+++ contentFiltering.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+df = pd.read_csv("article.csv")
+df = df[df['soup'].notna()]
+
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(df['soup'])
+
+
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+
+df = df.reset_index()
+indices = pd.Series(df.index, index=df['title'])
+
+def get_recommendations(title, cosine_sim):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    article_indices = [i[0] for i in sim_scores]
+    return df['url','title','text','lang','total_events'].iloc[article_indices].values.tolist()
